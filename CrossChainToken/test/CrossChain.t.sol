2eb9964ce597ed87625e6fed7f0af45fb1918ec5
GoldTeaaa <handayariawan@gmail.com>
2025-04-14 20:36:10 +0700

    commit until course 18

--- CrossChainToken/test/CrossChain.t.sol
+++ CrossChainToken/test/CrossChain.t.sol
@@ -7,14 +7,22 @@ import {RebaseToken} from "src/RebaseToken.sol";
 import {RebaseTokenPool} from "src/RebaseTokenPool.sol";
 import {Vault} from "src/Vault.sol";
 import {IRebaseToken} from "src/interfaces/IRebaseToken.sol";
+import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
 contract CrossChain is Test {
 
     uint256 sepoliaFork;
     uint256 arbSepoliaFork;
 
+    CCIPLocalSimulatorFork ccipLocalSimulatorFork;
+    
+
     function setUp() public {
+        // read the fork from foundry toml
         sepoliaFork = vm.createSelectFork("sepolia");
         arbSepoliaFork = vm.createFork("arb-sepolia");
+
+        ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        vm.makePersistent(address(ccipLocalSimulatorFork));
     }
 }
